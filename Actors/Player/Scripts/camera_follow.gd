0f8efdf5df8c2b798a710e2d318660f1ba370399
Passkii <neopetersson5@gmail.com>
2024-07-08 13:13:49 +0200

    remove camera follow thing, gonna improve camera now hehehehehehhehehehehehehehehehehehehehehehehehe

--- Actors/Player/Scripts/camera_follow.gd
+++ Actors/Player/Scripts/camera_follow.gd
@@ -1,8 +1,6 @@
 extends Camera2D
 
-@export var velCChangeMul: float = 0.258
-
 @onready var player = $"../2DPlayer"
 
 func _process(_delta) -> void:
-	global_position.x = player.global_position.x + (player.velocity.x * velCChangeMul)
+	global_position.x = player.global_position.x
